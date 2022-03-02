1ba55d7f16fb362e1dcff707267102a1bb2603b9
NikkiHmltn <nikkihmltn1@yahoo.com>
2022-03-02 00:19:10 -0800

    fonts added and sticky nav functionality

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("hello")
+$(document).ready(()=> {
+    $(window).scroll(() => {
+        if(window.scrollY > 300) {
+            $(".nav-container").addClass("fixed-nav")
+            navbar_height = $("nav").offsetHeight
+            $(document).css("padding-top", `${navbar_height} px`)
+        } else {
+            $(".nav-container").removeClass("fixed-nav")
+            $(document).css("padding-top", 0)
+        }
+    })
+})
